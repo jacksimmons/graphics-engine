93087a1e5325146f5fd4558a46ddc945b08f73f4
Jack Simmons <56202789+jacksimmons@users.noreply.github.com>
2025-02-04 14:15:11 +0000

    Cleaned up assimp folder & fixed imports

--- premake5.lua
+++ premake5.lua
@@ -30,7 +30,7 @@ project "Application"
     objdir "obj/%{cfg.buildcfg}"
     targetdir "bin/%{cfg.buildcfg}"
     libdirs { "bin/%{cfg.buildcfg}", "%{prj.name}/lib" }
-    links { "Engine", "opengl32", "glfw3", "glfw3dll", "assimp" }
+    links { "Engine", "opengl32", "glfw3", "glfw3dll", "assimp-vc143-mt" }
     defines { "GLM_ENABLE_EXPERIMENTAL", "FMT_UNICODE=0" }
     includedirs { "include",
         "Engine/src",
@@ -56,6 +56,6 @@ project "Application"
     prebuildcommands {
         "{COPYFILE} Engine/bin/%{cfg.buildcfg}/Engine.dll bin/%{cfg.buildcfg}/Engine.dll",
         "{COPYFILE} Engine/bin/%{cfg.buildcfg}/Engine.lib bin/%{cfg.buildcfg}/Engine.lib",
-        "{COPYFILE} vendor/assimp/lib/x64/assimp-vc143-mt.lib bin/%{cfg.buildcfg}/assimp-vc143-mt.lib",
-        "{COPYFILE} vendor/assimp/bin/x64/assimp-vc143-mt.dll bin/%{cfg.buildcfg}/assimp-vc143-mt.dll"
+        "{COPYFILE} vendor/assimp/lib/assimp-vc143-mt.lib bin/%{cfg.buildcfg}/assimp-vc143-mt.lib",
+        "{COPYFILE} vendor/assimp/bin/assimp-vc143-mt.dll bin/%{cfg.buildcfg}/assimp-vc143-mt.dll"
     }
