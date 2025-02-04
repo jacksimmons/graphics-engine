79c55ec08ce22fe0b039dd0129d425704afe22de
Jack Simmons <56202789+jacksimmons@users.noreply.github.com>
2025-02-04 10:05:34 +0000

    Added shader path input to inspector

--- Application/src/widget.cpp
+++ Application/src/widget.cpp
@@ -5,7 +5,18 @@
 
 namespace Tank
 {
-	void Widget::vec3Input(const char *label, glm::vec3 current, std::function<void(glm::vec3)> onModified)
+	void Widget::textInput(const char *label, const std::string &current, std::function<void(const std::string&)> onModified)
+	{
+		const size_t size = 100;
+		char buf[size] = "\0";
+		if (ImGui::InputTextWithHint(label, current.c_str(), buf, size))
+		{
+			onModified(std::string(buf));
+		}
+	}
+
+
+	void Widget::vec3Input(const char *label, glm::vec3 current, std::function<void(const glm::vec3&)> onModified)
 	{
 		float currentArr[] = { current.x, current.y, current.z };
 		if (ImGui::InputFloat3(label, currentArr))
