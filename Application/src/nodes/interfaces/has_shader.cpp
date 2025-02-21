a782f7bd108a3dbee81b558d8e226e00125da382
Jack Simmons <56202789+jacksimmons@users.noreply.github.com>
2025-02-21 13:16:24 +0000

    Changed static textures loaded to use weak_ptr
    This should prevent dangling textures after all models using them have been destroyed.

--- Application/src/nodes/interfaces/has_shader.cpp
+++ Application/src/nodes/interfaces/has_shader.cpp
@@ -1,13 +1,54 @@
+#include <algorithm>
 #include "has_shader.hpp"
 
 
 namespace Tank
 {
+	std::vector<std::weak_ptr<Texture>> IHasShader::s_loadedTextures;
+
+
 	IHasShader::IHasShader(const std::string &vsName,
 		const std::string &fsName)
 	{
 		auto maybeShader = Shader::createShader(vsName, fsName);
 		if (maybeShader.has_value())
 			m_shader = std::move(maybeShader.value());
+	}
+
+
+	void IHasShader::touchLoadedTextures()
+	{
+		std::vector<size_t> toRemoveIndices;
+		for (size_t i = 0; i < s_loadedTextures.size(); i++)
+		{
+			if (s_loadedTextures[i].expired()) toRemoveIndices.push_back(i);
+		}
+
+		for (const size_t toRemoveIndex : toRemoveIndices)
+		{
+			s_loadedTextures.erase(s_loadedTextures.begin() + toRemoveIndex);
+		}
+	}
+
+
+	std::vector<std::shared_ptr<Texture>> IHasShader::getLoadedTextures()
+	{
+		touchLoadedTextures();
+
+		std::vector<std::shared_ptr<Texture>> textures;
+		textures.resize(s_loadedTextures.size());
+		std::transform(
+			s_loadedTextures.begin(),
+			s_loadedTextures.end(),
+			textures.begin(),
+			[](std::weak_ptr<Texture> tex) { return tex.lock(); }
+		);
+		return textures;
+	}
+
+	
+	void IHasShader::addLoadedTexture(std::weak_ptr<Texture> texture)
+	{
+		s_loadedTextures.push_back(texture);
 	}
 }
--- Application/src/nodes/interfaces/has_shader.hpp
+++ Application/src/nodes/interfaces/has_shader.hpp
@@ -10,6 +10,16 @@ namespace Tank
 	class Texture;
 	class IHasShader
 	{
+	private:
+		// Non-owning reference to all loaded textures. Once all owners of a texture are
+		// destroyed, the corresponding entry in this vector becomes invalid.
+		static std::vector<std::weak_ptr<Texture>> s_loadedTextures;
+
+		/// <summary>
+		/// Checks all entries in loadedTextures for nullptr references, and removes any.
+		/// Only needed when retrieving data (getLoadedTextures).
+		/// </summary>
+		static void touchLoadedTextures();
 	protected:
 		std::unique_ptr<Shader> m_shader;
 
@@ -20,5 +30,8 @@ namespace Tank
 
 		void setShader(std::unique_ptr<Shader> shader) { m_shader = std::move(shader); };
 		Shader *getShader() const { return m_shader.get(); }
+
+		static std::vector<std::shared_ptr<Texture>> getLoadedTextures();
+		static void addLoadedTexture(std::weak_ptr<Texture> texture);
 	};
 }
